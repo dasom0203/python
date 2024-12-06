788507614287516d99137cd716485c6a139891c6
dasom <tlsektha3852@hanmail.net>
2024-12-06 15:15:06 +0900

    project

--- project/project.py
+++ project/project.py
@@ -1,11 +1,10 @@
-
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 # 첫 번째 CSV 파일 경로 (출산율)
 birth_file_path = "BirthAndMarriage.csv"
-# 두 번째 CSV 파일 경로 (유소년 인구 수)
+# 두 번째 CSV 파일 경로 (유소년 인구 수, 노령화 지수)
 old_file_path = "agingIndex.csv"
 # 한국어 사용을 위한 폰트 적용
 plt.rcParams['font.family'] ='Malgun Gothic'
@@ -13,8 +12,7 @@ plt.rcParams['font.family'] ='Malgun Gothic'
 # 데이터 배열 초기화
 years = []    # 연도
 births = []   # 출생 수
-##merries = []  # 혼인율
-olders = []   # 고령화 비율
+olders = []   # 노령화 지수
 yangs = [] # 유소년 인구
 
 
@@ -26,20 +24,18 @@ with open(birth_file_path, mode='r') as file:
     for row in reader:
         a = row[0]  # 첫 번째 값(연도)
         b = row[1]  # 두 번째 값(출생 수)
-##        c = row[-1]  # 세 번째 값(혼인율)
 
         years.append(int(a))
-        births.append(int(b)/1000)     
-##        merries.append(int(c)/1000) 
+        births.append(int(b)/1000) # 단위 조정 (천)  
 
-# 고령화 지수, 유소년 인구 데이터 읽기
+# 노령화 지수, 유소년 인구 데이터 읽기
 with open(old_file_path, mode='r') as file:
     reader = csv.reader(file)  # CSV 파일을 읽기 위한 객체 생성
     header = next(reader)      # 첫 번째 줄은 헤더이므로 건너뜀
 
     for row in reader:
         a = row[0]  # 첫 번째 값(연도)
-        b = row[-4]  # 두 번째 값(노령화 지수)
+        b = row[-4]  # 두 번째 값(노령화 지수), 노령화지수=고령인구/유소년인구 * 100
         c = int(row[4]) / int(row[1]) *100  # 유소년 비율 (유소년 수/전체인구*100)
 
         # 이미 존재하는 연도에 대해서만 추가
@@ -55,20 +51,19 @@ ax1.bar(years, births, color='tan', width=0.5, label='출생 수', alpha=0.6)
 
 # 두 번째 y축을 설정 (출생 수와 유소년 비율, 노령화 지수가 다른 범위의 값을 가지므로)
 ax2 = ax1.twinx()
-
-# 두 번째 y축을 기준으로 혼인율을 선 그래프(line plot)로 표시
+# 두 번째 y축을 기준으로 유소년 비율을 선 그래프(line plot)로 표시
 ax2.plot(years, yangs, color='r', marker='o', label='유소년 인구 비율', linewidth=2)
-
-
 # y축을 백분율로 표시
 plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(decimals=0))
+
 
 # 세 번째 y축을 기준으로 노령화 지수를 선 그래프(line plot)로 표시
 ax3 = ax1.twinx()  # 세 번째 y축 추가
 ax3.spines['right'].set_position(('outward', 60))  # 세 번째 y축 위치를 오른쪽으로 이동
 ax3.plot(years, olders, color='b', label='노령화 지수', linewidth=2)
 
-# 첫 번째 y축 레이블 설정
+
+# 축 레이블 설정
 ax1.set_xlabel('연도')   # x축은 연도를 표시
 ax1.set_ylabel('출생 수 (단위 : 천)')   # 첫 번째 y축은 출생 수
 ax2.set_ylabel('유소년 인구 비율') # 두 번째 y축은 유소년 인구 비율
@@ -80,7 +75,18 @@ fig.legend(loc='upper center',  bbox_to_anchor=(0.5, 0.87),ncol=3)
 
 
 # 그래프 제목 설정
-plt.title("결혼과 출산율의 감소로 인한 노령화 지수 상승", fontsize=15)
+plt.title("출산율 및 유소년 비율의 감소로 인한 노령화 지수 상승", fontsize=15)
 
 # 그래프 출력
 plt.show()
+
+
+"""
+# numpy를 사용해 상관계수 계산
+corr_birth_aging = np.corrcoef(births, olders)[0, 1]
+corr_young_aging = np.corrcoef(yangs, olders)[0, 1]
+
+print("출산율과 노령화 지수 간 상관계수:", corr_birth_aging)
+print("유소년 비율과 노령화 지수 간 상관계수:", corr_young_aging)
+"""
+
